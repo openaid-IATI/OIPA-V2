e5e466deebbc6b07a207049e1f92ec804556ba52
intelliadmin <h.vanderheide@intellimedia.nl>
2012-07-02 16:47:30 +0200

    added deployment settings

--- iati/urls.py
+++ iati/urls.py
@@ -1,15 +1,19 @@
 # Django specific
+from django.conf import settings
 from django.conf.urls import *
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-from django.conf import settings
+
+from api.v2.urls import api_v2_docs
 
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
     (r'^admin/', include(admin.site.urls)),
+
+    (r'^$', api_v2_docs),
 
     (r'^api/', include('api.urls')),
 )
