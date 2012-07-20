a1ea3b7d4190a0bd3bee6b5f2cd3aaae82dc83b2
intelliadmin <h.vanderheide@intellimedia.nl>
2012-07-20 15:15:48 +0200

    api consistency fix

--- iati/api/v2/resources/model_resources.py
+++ iati/api/v2/resources/model_resources.py
@@ -62,7 +62,7 @@ class ActivityResource(ModelResource):
     activity_status = fields.ForeignKey(StatusResource, attribute='activity_status', full=True, null=True)
     recipient_country = fields.ToManyField(RecipientCountryResource, 'iatiactivitycountry_set', full=True, null=True)
     recipient_region = fields.ToManyField(RecipientRegionResource, 'iatiactivityregion_set', full=True, null=True)
-    sector = fields.ToManyField(SectorResource, 'sectors', full=True, null=True)
+    sectors = fields.ToManyField(SectorResource, 'sectors', full=True, null=True)
     collaboration_type = fields.ForeignKey(CollaborationTypeResource, attribute='collaboration_type', full=True, null=True)
     default_flow_type = fields.ForeignKey(FlowTypeResource, attribute='default_flow_type', full=True, null=True)
     default_finance_type = fields.ForeignKey(FinanceTypeResource, attribute='default_finance_type', full=True, null=True)
@@ -77,7 +77,7 @@ class ActivityResource(ModelResource):
         resource_name = 'activities'
         serializer = Serializer(formats=['xml', 'json'])
         excludes = ['date_created']
-        ordering = ['start_actual', 'start_planned', 'sector', 'statistics']
+        ordering = ['start_actual', 'start_planned', 'sectors', 'statistics']
         filtering = {
             # example to allow field specific filtering.
 #            'reporting_organisation': ALL_WITH_RELATIONS,
