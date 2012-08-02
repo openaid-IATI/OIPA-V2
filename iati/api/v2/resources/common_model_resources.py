6dba63571e3c24158a38bf61df121a49575c8801
intelliadmin <h.vanderheide@intellimedia.nl>
2012-08-02 14:27:16 +0200

    fixtures

--- iati/api/v2/resources/common_model_resources.py
+++ iati/api/v2/resources/common_model_resources.py
@@ -53,10 +53,10 @@ class SectorResource(ModelResource):
 
 
 class LanguageResource(ModelResource):
-    include_resource_uri = False
-    excludes = ['id']
     class Meta:
         queryset = Language.objects.all()
+        include_resource_uri = False
+        excludes = ['id']
 
 
 class TitleResource(ModelResource):
