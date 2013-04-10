80b577f8da9e23fdbeb2e05dbe51def66a6b61a0
berjan <berjan@zimmermanzimmerman.nl>
2013-04-10 11:18:02 +0200

    -add support filters api UN

--- iati/utils/views.py
+++ iati/utils/views.py
@@ -64,15 +64,65 @@ class UploadUnHabitatIndicatorCountryCSV(FormView):
     def form_valid(self, form):
         form.save()
         return super(UploadUnHabitatIndicatorCountryCSV, self).form_valid(form=form)
+def make_where_query(values, name):
+    query = ''
+    if not values[0]:
+        return None
+
+    for v in values:
+        query += '  ' + name + ' = "' + v +'" OR'
+    query = query[:-2]
+    return query
 
 def test_json_response(request):
+
+    country_filters = request.GET.get('countries', None)
+    if country_filters:
+        country_q = make_where_query(values=country_filters.split(','), name='iso')
+        country_q += ') AND ('
+    else:
+        country_q = ''
+
+    region_filters = request.GET.get('regions', None)
+    if region_filters:
+        region_q = make_where_query(values=region_filters.split(','), name='dac_region_code')
+        region_q += ') AND ('
+    else:
+        region_q = ''
+
+    indicator_filters = request.GET.get('indicator', None)
+    if indicator_filters:
+        indicator_q = make_where_query(values=indicator_filters.split(','), name='indicator_id')
+        indicator_q += ') AND ('
+    else:
+        indicator_q = ''
+
+    if not indicator_q:
+        indicator_q = ' indicator_id = "population"'
+
+    filter_string = '  (' + country_q + region_q + indicator_q + ')'
+
+    if 'AND ()' in filter_string:
+        filter_string = filter_string[:-6]
+
     cursor = connection.cursor()
 
-    cursor.execute('SELECT indicator_id, country_id, country_name, dac_region_code, dac_region_name, value, year, latitude, longitude '
-                   'FROM data_indicatordata id LEFT OUTER JOIN data_country C ON id.country_id=C.iso '
-                   'WHERE indicator_id = \"population\"')
+    cursor.execute('SELECT indicator_id, id.country_id,  country_name, dac_region_code, dac_region_name, value, year, latitude, longitude, C.iso '
+                   'FROM data_indicatordata id LEFT OUTER JOIN data_country C ON id.country_id=C.iso  '
+                   'WHERE %s' % (filter_string))
+    #LEFT OUTER JOIN data_city City ON C.iso = City.country_id
     cursor_max = connection.cursor()
-    cursor_max.execute('SELECT max(value) as max_value FROM data_indicatordata')
+
+    if indicator_filters:
+        indicator_q = make_where_query(values=indicator_filters.split(','), name='indicator_id')
+#        indicator_q += ' AND '
+    else:
+        indicator_q = ''
+
+    if not indicator_q:
+        indicator_q = ' indicator_id = "population"'
+
+    cursor_max.execute('SELECT max(value) as max_value FROM data_indicatordata WHERE %s' % indicator_q)
     result_max = cursor_max.fetchone()
     desc = cursor.description
     results = [
@@ -92,28 +142,31 @@ def test_json_response(request):
         #        year[r['year']] = r['value']
 #        years.append(year)
 #        try:
-        try:
+        #check if it has a result
+        if r['value']:
+            try:
+                years = country[r['country_id']]['years']
+            except:
+                country[r['country_id']] = {'name' : r['country_name'], 'longitude' : r['longitude'], 'latitude' : r['latitude'], 'max_value' : result_max[0], 'years' : {}}
+
+
+
             years = country[r['country_id']]['years']
-        except:
-            country[r['country_id']] = {'name' : r['country_name'], 'longitude' : r['longitude'], 'latitude' : r['latitude'], 'max_value' : result_max[0], 'years' : {}}
+            year['y' + str(r['year'])] = r['value']
 
 
+            country[r['country_id']]['years'].update(year)
 
-        years = country[r['country_id']]['years']
-        year['y' + str(r['year'])] = r['value']
+            region = {}
+            if r['dac_region_code']:
+                region[r['dac_region_code']] = r['dac_region_name']
+                regions.update(region)
 
+            cntry = {}
+            if r['country_id']:
+                cntry[r['country_id']] = r['country_name']
+                countries.update(cntry)
 
-        country[r['country_id']]['years'].update(year)
-
-        region = {}
-        if r['dac_region_code']:
-            region[r['dac_region_code']] = r['dac_region_name']
-            regions.update(region)
-
-        cntry = {}
-        if r['country_id']:
-            cntry[r['country_id']] = r['country_name']
-            countries.update(cntry)
 
     country['regions'] = regions
     country['countries'] = countries
@@ -143,15 +196,7 @@ def test_json_response(request):
 
     return HttpResponse(json.dumps(country), mimetype='application/json')
 
-def make_where_query(values, name):
-    query = ''
-    if not values[0]:
-        return None
 
-    for v in values:
-        query += '  ' + name + ' = "' + v +'" OR'
-    query = query[:-2]
-    return query
 def test_json_city_response(request):
     city_filters = request.GET.get('city', None)
     if city_filters:
@@ -196,6 +241,20 @@ def test_json_city_response(request):
     for row in cursor.fetchall()
     ]
 
+    cursor_max = connection.cursor()
+
+    if indicator_filters:
+        indicator_q = make_where_query(values=indicator_filters.split(','), name='indicator_id')
+    #        indicator_q += ' AND '
+    else:
+        indicator_q = ''
+
+    if not indicator_q:
+        indicator_q = ' indicator_id = "cpi_5_dimensions"'
+
+    cursor_max.execute('SELECT max(value) as max_value FROM data_indicatorcitydata WHERE %s' % indicator_q)
+    result_max = cursor_max.fetchone()
+
     country = {}
     regions = {}
     countries = {}
@@ -205,7 +264,7 @@ def test_json_city_response(request):
         try:
             country[r['city_id']]['years']
         except:
-            country[r['city_id']] = {'name' : r['name'], 'longitude' : r['longitude'], 'latitude' : r['latitude'], 'years' : {}}
+            country[r['city_id']] = {'name' : r['name'], 'longitude' : r['longitude'], 'latitude' : r['latitude'], 'max_value' : result_max[0], 'years' : {}}
 
 #        years = country[r['city_id']]['years']
         year['y' + str(r['year'])] = r['value']
