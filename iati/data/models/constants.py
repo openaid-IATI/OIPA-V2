9f607840fcd91a69c0b534b6ef9dafb5cc60e487
berjan <berjan@zimmermanzimmerman.nl>
2013-04-22 17:23:30 +0200

    -fix geo locations and iso2

--- iati/data/models/constants.py
+++ iati/data/models/constants.py
@@ -345,7 +345,7 @@ COUNTRY_LOCATION = {
     "FK": {"longitude": "-51.45", "latitude": "-59.00"},
     "FM": {"longitude": "6.55", "latitude": "158.15"},
     "FO": {"longitude": "62.00", "latitude": "-7.00"},
-    "FR": {"longitude": "47.06", "latitude": "2.17"},
+    "FR": {"longitude": "2.17", "latitude": "47.06"},
     "GA": {"longitude": "-1.00", "latitude": "11.45"},
     "GB": {"longitude": "54.00", "latitude": "-2.00"},
     "GD": {"longitude": "12.07", "latitude": "-61.40"},
